58176a97d09212c39034652c92c11cdb9506450e
deng3shui <deng3shui@outlook.com>
2021-11-24 17:11:35 +0800

    feat

--- pokemon/src/views/Game.vue
+++ pokemon/src/views/Game.vue
@@ -3,46 +3,65 @@
   <el-header>
     <el-progress style="width:600px" :text-inside="true" :stroke-width="24" :percentage="100" status="success"></el-progress>
   </el-header>
+  <h1 v-show="showName()">{{correctAnswer}}111</h1>
   <el-main>
-    <h1>g{{gameId}}</h1>
-    <img :src=gameId>
+    <img :src=pokemon.src alt="">
   </el-main>
-  <el-foot>
-    <el-radio-group v-model="radio">
-      <el-radio :label="3">备选项</el-radio>
-      <el-radio :label="6">备选项</el-radio>
-      <el-radio :label="9">备选项</el-radio>
-      <el-radio :label="12">备选项</el-radio>
+  <el-footer>
+    <el-radio-group v-model="radio" @change = 'showName(radio)'>
+      <el-radio :label=pokemon.option[0]>{{ pokemon.option[0] }}</el-radio>
+      <el-radio :label=pokemon.option[1]>{{ pokemon.option[1] }}</el-radio>
+      <el-radio :label=pokemon.option[2]>{{ pokemon.option[2] }}</el-radio>
+      <el-radio :label=pokemon.option[3]>{{ pokemon.option[3] }}</el-radio>
     </el-radio-group>
-  </el-foot>
+  </el-footer>
 </div>
 </template>
 
 <script>
 import PokemonCard from "@/components/PokemonCard";
-let gameId = ''
+let pokemon = {}
+let correctAnswer = ''
 export default {
   name: "Game",
   components: {PokemonCard},
   data(){
     return{
-      gameId,
+      pokemon,
       radio:0,
-      a:Math.round(Math.random()*890)
+      correctAnswer,
 
     }
   },
   methods:{
     creatId(){
      let num = Math.round(Math.random()*890)
-      this.gameId = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${num}.png`
-      console.log(this.gameId)
+      this.pokemon.id = num
+      this.pokemon.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${num}.png`
+      this.pokemon.option = [
+        JSON.parse(localStorage.getItem(Math.round(Math.random()*890).toString())).name,
+        JSON.parse(localStorage.getItem(Math.round(Math.random()*890).toString())).name,
+        JSON.parse(localStorage.getItem(Math.round(Math.random()*890).toString())).name,
+        JSON.parse(localStorage.getItem(Math.round(Math.random()*890).toString())).name]
+      this.pokemon.option[Math.round(Math.random()*4)] = JSON.parse(localStorage.getItem(num.toString())).name
+      this.correctAnswer = JSON.parse(localStorage.getItem(num.toString())).name
+
+
+    },
+    showName(radio){
+      let imgIsShow
+      if (radio===window.correctAnswer){
+        console.log('99')
+        imgIsShow = true
+      }
+      else  imgIsShow = false
+      return imgIsShow
     },
 
   },
   beforeMount(){
     this.creatId()
-    console.log('99')
+
   },
 }
 
